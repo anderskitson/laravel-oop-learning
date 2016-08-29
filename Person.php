6958499f778a7094eb63413eec5d231f6c80854e
Anders Kitson <anders.kitson@canadadrives.ca>
2016-08-29 11:49:07 -0700

    lesson 2 getters and setters first test working

---
+++ Person.php
@@ -0,0 +1,30 @@
+<?php
+
+class Person{
+
+    public $name;
+
+    public $age;
+
+    Public function __construct($name){
+        $this->name = $name;
+    }
+
+    Public function getAge(){
+        return $this->age;
+    }
+
+    Public function setAge($age){
+
+        if ($age < 18){
+            throw new Exception("Person is not old Enough");
+        }
+
+        $this->age = $age;
+    }
+}
+
+$john = new Person('John Doe');
+$john->setAge(30);
+
+var_dump($john->getAge());
